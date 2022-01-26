ce58154d7968b5de4266fa10725458c4d1d945b2
Lucas Borges Dalcin <lucasborgesdalcin@gmail.com>
2022-01-26 07:15:23 -0300

    removido o comando 420

--- index.js
+++ index.js
@@ -31,13 +31,10 @@ client.on("message", (message) =>{
 	const argument = message.content.slice(prefix.length)
 
 	switch(argument) {
-		case "help": message.channel.send ("Comandos Disponiveis:\n//oi\n//420\n//69\n//gay\n//windows")
+		case "help": message.channel.send ("Comandos Disponiveis:\n//oi\n//69\n//gay\n//windows")
 		break;
 
 		case "oi": message.channel.send ("Olá :wave:")
-		break;
-
-		case "420": message.channel.send ("Smoke Weed Everyday")
 		break;
 
 		case "69": message.channel.send ("Eta bixo secso kkkkk")
@@ -46,10 +43,10 @@ client.on("message", (message) =>{
 		case "gay": message.channel.send ("https://pics.me.me/ur-the-biggest-gay-36954314.png")
 		break;
 		
-		case "windows": message.channel.send ("O cara usa windows mano mt troxa pqp kkkkkk")
+		case "windows": message.channel.send ("O cara usa windows mano, mt troxa pqp kkkkkk")
 		break;
 
-		default: message.channel.send("Ocê é burro carai? Tem esse comando não, digita //help aí.")
+		default: message.channel.send("Comando não reconhecido")
 		break;
 	}
 })
