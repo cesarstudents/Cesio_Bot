9f6cf5dd748404075c74db4b247496e438f6d2b1
Lucas Borges Dalcin <lucasborgesdalcin@gmail.com>
2022-01-26 22:56:52 -0300

    adicionei comando do mucalol com memes aleatorios

--- commands.js
+++ commands.js
@@ -26,6 +26,12 @@ module.exports = client => {
 			case "windows": message.channel.send ("O cara usa windows mano, mt troxa pqp kkkkkk")
 			break;
 
+			case "mucalol": 
+			const messages = ["https://pm1.narvii.com/7639/5804f9d44393dc2f37888c9b410cf2a8113dc469r1-487-630v2_hq.jpg", "https://electron.scrolller.com/atto/why-so-serius-e35wgoodei-540x405.jpg", "https://pm1.narvii.com/7639/ec89f3aabb05ac8519dfa6f566c11da49f6f1e72r1-1200-602v2_hq.jpg", "https://pm1.narvii.com/7639/f3a77c6d030b6b17f28086c83b144a5928fa9d9br1-554-554v2_hq.jpg"]
+			const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+			message.channel.send(randomMessage)
+			break;
+
 			default: message.channel.send("Comando não reconhecido")
 			break;
 		}
